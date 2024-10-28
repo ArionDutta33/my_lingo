2b7044cccf96085c6c90abaac77a52da6cb667c0
ArionDutta33 <ariondutta333@outlook.com>
2024-10-28 20:10:53 +0530

    UI state management

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,18 @@
 import { Stack, Link } from 'expo-router';
-import { Text, TextInput, View } from 'react-native';
+import { Pressable, Text, TextInput, View } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Button } from '~/components/Button';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import Feather from '@expo/vector-icons/Feather';
+import { useState } from 'react';
 export default function Home() {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+
+  const onTranslate = () => {
+    const translation = input;
+    setOutput(translation);
+  };
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
@@ -16,25 +24,31 @@ export default function Home() {
       <View className="border-y border-gray-300 p-3">
         <View className="i flex-row  gap-5">
           <TextInput
+            value={input}
+            onChangeText={(val) => setInput(val)}
             placeholder="Enter your text"
             className="min-h-32 flex-1  text-lg"
             multiline={true}
             maxLength={5000}
           />
-          <FontAwesome6 name="circle-arrow-right" size={24} color="royalblue" />
+          <Pressable onPress={onTranslate}>
+            <FontAwesome6 name="circle-arrow-right" size={24} color="royalblue" />
+          </Pressable>
         </View>
         <View className="flex-row justify-between">
           <FontAwesome name="microphone" size={18} color="dimgray" />
-          <Text className="text-gray-500">0/5000</Text>
+          <Text className="text-gray-500">{input.length}/5000</Text>
         </View>
       </View>
-      <View className="gap-5 bg-gray-300 p-5">
-        <Text className="min-h-32   text-lg">Ouput</Text>
-        <View className="flex-row justify-between">
-          <FontAwesome6 name="volume-high" size={18} color="dimgray" />
-          <Feather name="clipboard" size={18} color="dimgray" />
+      {output && (
+        <View className="gap-5 bg-gray-300 p-5">
+          <Text className="min-h-32   text-lg">{output}</Text>
+          <View className="flex-row justify-between">
+            <FontAwesome6 name="volume-high" size={18} color="dimgray" />
+            <Feather name="clipboard" size={18} color="dimgray" />
+          </View>
         </View>
-      </View>
+      )}
     </>
   );
 }
